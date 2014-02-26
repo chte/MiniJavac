adf06582e74d04ac585ffab95c22e6c43804f07a
Carl Eriksson <carle293@hotmail.com>
2014-02-26 16:56:47 +0100

    Improved test suite

--- testsuit/runTest.sh
+++ testsuit/runTest.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-
 echo "==COMPILE=="
 #Test that should compile
 FILES=./testsuit/compile/*
 #Iterate over the files
 for f in $FILES
 do
-	echo "File: $f"
+	#echo "File: $f"
   	OUTPUT=$(java -jar ./build/jar/MiniJava.jar $f)
   	RESULT=$(echo $OUTPUT | tail -c 49)
 
   	if [ "$RESULT"  == "MiniJavac 1.0: Java program parsed successfully." ]; 
   	then
-  	    echo "[OK] $f"
+  	    echo "[OK] - $(basename $f)"
 	else
-	    echo "[FAIL] $f"
+	    echo "[FAIL] - $(basename $f)"
 	fi
 done
 
@@ -27,14 +26,14 @@ FILES=./testsuit/noncompile/*
 #Iterate over the files
 for f in $FILES
 do
-	echo "File: $f"
+	#echo "File: $f"
   	OUTPUT=$(java -jar ./build/jar/MiniJava.jar $f)
   	RESULT=$(echo $OUTPUT | tail -c 49)
 
   	if [ "$RESULT"  == "MiniJavac 1.0: Java program parsed successfully." ]; 
   	then
-  	    echo "[FAIL] $f"
+  	    echo "[FAIL] - $(basename $f)"
 	else
-	    echo "[OK] $f"
+	    echo "[OK] - $(basename $f)"
 	fi
 done
