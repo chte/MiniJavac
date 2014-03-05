a76f81e92f458cca6c153e2a44497cb24ccb19cc
chte <chte@kth.se>
2014-03-05 16:12:50 +0100

    Built code for generating syntax tree, visitor package still needs to be coded

--- src/syntaxtree/MainClass.java
+++ src/syntaxtree/MainClass.java
@@ -4,10 +4,11 @@ import visitor.TypeVisitor;
 
 public class MainClass {
   public Identifier i1,i2;
-  public Statement s;
+  public StatementList sl;
+  public VarDeclList vdl;
 
-  public MainClass(Identifier ai1, Identifier ai2, Statement as) {
-    i1=ai1; i2=ai2; s=as;
+  public MainClass(Identifier ai1, Identifier ai2, VarDeclList avdl, StatementList asl) {
+    i1=ai1; i2=ai2; vdl=avdl; sl=asl;
   }
 
   public void accept(Visitor v) {
