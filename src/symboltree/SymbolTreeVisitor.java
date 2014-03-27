6245d1d5bcc1dddd2f681d19638a63eb7602f51f
Carl Eriksson <carle293@hotmail.com>
2014-03-27 16:13:47 +0100

    Symboltable work in progress

--- src/symboltree/SymbolTreeVisitor.java
+++ src/symboltree/SymbolTreeVisitor.java
@@ -2,7 +2,7 @@ package symboltree;
 
 import syntaxtree.*;
 
-public class SymbolTreeVisitor implements Visitor{
+public class SymbolTreeVisitor implements visitor.DepthFirstVisitor{
 
 	public SymbolTable ROOT;
 
@@ -10,7 +10,31 @@ public class SymbolTreeVisitor implements Visitor{
 		ROOT = new SymbolTable(null);
 	}
 
-	public void visit(Program n){
-		
-	}	
+	//Start the program 
+	public void visit(Program n, SymbolTable parent){
+		super.visit(n);
+	}
+
+	//Main program
+	public void visit(MainClass n, SymbolTable parent){
+		//Create the new scope with the parent as parent
+		SymbolTable child = new SymbolTable(parent);
+		//Add the child to the parent
+		this.parent.addChild(this.child);
+			
+		super.visit(n);
+	}
+
+	private String nodeName(Object o){
+		String name = o.getClass().toString();
+		int dot = name.lastIndexOf(".");
+		if (dot != -1){
+			name = name.substring(dot+1);
+		}
+		return name;	
+    }
+
+    private void print(String toPrint){
+    	System.out.println("STV: " + toPrint);
+    }
 }
